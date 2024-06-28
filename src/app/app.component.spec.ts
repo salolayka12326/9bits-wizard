76e4a84e2c202bcca344389003bdefeee965d356
Oleg Bozhkov <mr.bozhkov12326@gmail.com>
2024-06-28 18:42:17 +0300

    Init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, steps-17-no-standalone');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 9bits-wizards');
   });
 });
